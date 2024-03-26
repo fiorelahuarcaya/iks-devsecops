23ef8ff48bfd547f6366afac023687e4b6b06e4b
Fiorela Huarcaya <65567785+fiorelahuarcaya@users.noreply.github.com>
2024-03-25 23:40:07 -0500

    Update hello.ts

--- src/hello.ts
+++ src/hello.ts
@@ -7,7 +7,7 @@ export function buildFastify(logger = false) {
 
   // Declare a route
   fastify.get("/", async function handler(request, reply) {
-    return { message: "HOLA ssl" };
+    return { message: "HOLA hola" };
   });
 
   return fastify;
