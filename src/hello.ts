faf266cdef109770f322c0fa01ce4f4a17e86769
Fiorela Huarcaya Gonzales <Fiorela.Huarcaya@ibm.com>
2024-05-06 15:58:38 -0500

    edit

--- src/hello.ts
+++ src/hello.ts
@@ -7,7 +7,7 @@ export function buildFastify(logger = false) {
 
   // Declare a route
   fastify.get("/", async function handler(request, reply) {
-    return {  hello: "HOLA"  };
+    return {  hello: "hello"  };
   });
 
   return fastify;
